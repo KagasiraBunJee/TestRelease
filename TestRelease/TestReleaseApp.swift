bf80c6aff9e5873baefc687767fc2756241244a6
Sergii Polishchuk <kagasirabunjee@yahoo.com>
2026-05-15 19:07:52 +0300

    Initial Commit

---
+++ TestRelease/TestReleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReleaseApp.swift
+//  TestRelease
+//
+//  Created by Sergii Polishchuk on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct TestReleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
